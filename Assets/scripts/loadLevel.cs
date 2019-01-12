dd74d71bda2f40916cf4f482660853f5f53640b1
LEARNET\elmer.rastamwase <elmer.rstamwase@elev.ga.lbs.se>
2019-01-12 21:15:27 +0100

    added tutorial and menu

--- Assets/scripts/loadLevel.cs
+++ Assets/scripts/loadLevel.cs
@@ -8,7 +8,7 @@ public class loadLevel : MonoBehaviour
     // variabel för hur mycker scare man behöver för att triggera level loadern
     public int minScore;
     // variabel för vilken level som ska loadas (lv1 deffult)
-    public string levelLoad = "elmerPlatformer1_scene1";
+    public string levelToLoad = "elmerPlatformer1_scene1";
 
     // kollar ifall någonting rör sakens trigger
     private void OnTriggerEnter2D(Collider2D collision)
@@ -16,12 +16,19 @@ public class loadLevel : MonoBehaviour
         // kollar ifall saken som kolliderar är taggad med "Player" och ifall man har minimum
         if (collision.tag == "Player" && coin.score >= minScore)
         {
-            // resettar spelarens score när en level loadas (dör ellar går in i nästa bana) 
-            coin.score = 0;
-            // loadar scen beroende på 'levelLoad'
-            SceneManager.LoadScene(levelLoad);
-            // playern förlorar sin powerup
-            powerup.playerState = 1;
+            //spelar levelLoad voiden
+            levelLoad();
+            
         }
+    }
+
+    public void levelLoad()
+    {
+        // resettar spelarens score när en level loadas (dör ellar går in i nästa bana) 
+        coin.score = 0;
+        // loadar scen beroende på 'levelLoad'
+        SceneManager.LoadScene(levelToLoad);
+        // playern förlorar sin powerup
+        powerup.playerState = 1;
     }
 }
